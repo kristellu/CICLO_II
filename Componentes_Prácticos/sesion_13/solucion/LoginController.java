4c19cc5fd5e482dbcb60d15913afa007b25e9ce0
kristellu <kristellu@uninorte.edu.co>
2021-07-23 10:48:47 -0500

    CP: 13, 14 y 15

---
+++ Componentes_Prácticos/sesion_13/solucion/LoginController.java
@@ -0,0 +1,19 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    void register(ActionEvent event) {
+
+    }
+
+}
